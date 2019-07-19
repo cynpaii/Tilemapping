5fcea86297092a17e8f7f8ab1487d950627af3d6
Cynpaii <cynthiachu@knights.ucf.edu>
2019-07-19 15:49:06 -0400

    challenge

---
+++ Tilemap/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 1.0f;
+     public float x1;
+     public float y1;
+     public float x2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Vector2 pos1 = new Vector2(x1, y1);
+
+        Vector2 pos2 = new Vector2(x2, y1);
+
+        transform.position = Vector2.Lerp (pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f); 
+    }
+    
+}
--- Tilemap/Assets/Scripts/PlayerController.cs
+++ Tilemap/Assets/Scripts/PlayerController.cs
@@ -1,41 +1,118 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
- 
     private Rigidbody2D rb2d;
+    private int score;
+    private int lives;
     public float speed;
-    public float jumpForce;
+    public Text winText;
     
+    public Text livesText;
+    public Text loseText;
+
+    public Text scoreText;
+
+    public float jumpForce;
+
+
+
+
     // Use this for initialization
     void Start () {
-        rb2d= GetComponent<Rigidbody2D>();
+
+        rb2d = GetComponent<Rigidbody2D>();
+        score = 0;
+        lives = 3;
+        winText.text = "";
+        loseText.text = "";
+        SetScoreText ();
+        SetLivesText ();
+    
+        
+       
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update () 
+        {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
     }
+
+    
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(moveHorizontal, 0); 
-        rb2d.AddForce(movement * speed);
 
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
     }
-    
+
     void OnCollisionStay2D(Collision2D collision)
     {
         if(collision.collider.tag == "Ground") {
+
             if(Input.GetKey(KeyCode.UpArrow)) {
-                rb2d.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse);
+
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
         }
-        
     }
- }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+        other.gameObject.SetActive(false);
+
+        score = score + 1;
+
+        SetScoreText ();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+        other.gameObject.SetActive(false);
+
+        lives = lives - 1;
+        SetLivesText ();
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString ();
+
+        if (score == 4)
+            {
+            transform.position = new Vector2(35f, 18f);
+            lives=3;
+            }
+        if (score >= 8)
+        {
+            winText.text = "You Win!";
+            FindObjectOfType<AudioManager>().Play("Mushroom Theme");
+           
+        }
+    }
+    
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString ();
+
+        if (lives <= 0)
+            loseText.text = "You Lose!";
+
+        if (lives == 0)
+            Destroy(gameObject);
+    }
+}
+
